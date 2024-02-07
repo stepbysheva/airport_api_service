e19d59aa9e57c9f5b3fe87e357be51285de48320
Stepan Shevchuk <stepka.shevchuk@gmail.com>
2024-02-07 21:09:07 +0000

    Adding apps to settings.py

--- airport_api_service/settings.py
+++ airport_api_service/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "airoport_service",
+    "rest_framework"
 ]
 
 MIDDLEWARE = [
