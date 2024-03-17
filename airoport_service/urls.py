da9b6d34e1ccc29915305d703a611b88d1a64d88
Stepan Shevchuk <stepka.shevchuk@gmail.com>
2024-03-17 20:45:35 +0000

    Adding views for all endpoints

--- airoport_service/urls.py
+++ airoport_service/urls.py
@@ -6,17 +6,19 @@ from airoport_service.views import AirplaneViewSet, AirplaneTypeViewSet, Airport
 
 router = routers.DefaultRouter()
 
-router.register("airplanes", AirplaneViewSet)
-router.register("airplane_types", AirplaneTypeViewSet)
-router.register("airports", AirportViewSet)
-router.register("routes", RouteViewSet)
-router.register("crews", CrewViewSet)
+
 router.register("flights", FlightViewSet)
 router.register("orders", OrderViewSet)
 
 
 urlpatterns = [
-    path("", include(router.urls))
+    path("", include(router.urls)),
+    path("airplanes/", AirplaneViewSet.as_view()),
+    path("airplane_types/", AirplaneTypeViewSet.as_view()),
+    path("airports/", AirportViewSet.as_view()),
+    path("routes/", RouteViewSet.as_view()),
+    path("crews/", CrewViewSet.as_view())
+
 ]
 
 
