7acba1a43b80c8e4f9d1ce9e63b45729a88842ff
Stepan Shevchuk <stepka.shevchuk@gmail.com>
2024-03-18 20:44:23 +0000

    Adding docker

--- airport_api_service/settings.py
+++ airport_api_service/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+import os
 from datetime import timedelta
 from pathlib import Path
 
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w8$b5$b+=85&f+u)ii&cw$8fp1u44(l*iz#o)hi*%n+7m-y%!2'
+SECRET_KEY = os.environ.get("SECRET_KEY", 'django-insecure-w8$b5$b+=85&f+u)ii&cw$8fp1u44(l*iz#o)hi*%n+7m-y%!2')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -79,9 +80,12 @@ WSGI_APPLICATION = 'airport_api_service.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "HOST": os.environ["POSTGRES_HOST"],
+        "NAME": os.environ["POSTGRES_DB"],
+        "USER": os.environ["POSTGRES_USER"],
+        "PASSWORD": os.environ["POSTGRES_PASSWORD"],
     }
 }
 
